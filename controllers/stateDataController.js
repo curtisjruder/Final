8bec15965f138f31fdca5b1df00e7dcaae26d976
Curtis Ruder HP <curtisjruder@gmail.com>
2022-04-30 14:45:32 -0500

    This... is... it! WOO

--- controllers/stateDataController.js
+++ controllers/stateDataController.js
@@ -30,7 +30,12 @@ const getStates = async (req, res) => {
 const getState = async (req, res) => {
     await configFacts()
     let x = data.states.find(itm => {return itm.code === req.stateCode})
-    return res.json(x);
+    if(!x.funfacts || x.funfacts.length === 0) return res.json({"message" : "No Fun Facts found for " + req.stateName})
+    
+    let i = Math.floor(Math.random() * x.funfacts.length);
+    return res.json({
+        "funfact" : x.funfacts[i]
+    })
 }
 
 const getCapital = (req, res) => {
--- routes/funfactRoute.js
+++ routes/funfactRoute.js
@@ -4,17 +4,7 @@ const {getState} = require("../controllers/stateDataController");
 const {addFact, editFact, deleteFact} = require("../controllers/factsController");
 
 router.route("/")
-    .get((req,res) => {
-        // find the state
-        let returnVal = getState(req.stateCode);   
-
-        if(!returnVal.funfacts || returnVal.funfacts.length === 0) return res.json({"message" : "No Fun Facts found for " + returnVal.state})
-
-        let i = Math.floor(Math.random() * returnVal.funfacts.length);
-        return res.json({
-            "funfact" : returnVal.funfacts[i]
-        })
-    })
+    .get(getState)
     .post(addFact)
     .patch(editFact)
     .delete(deleteFact)
